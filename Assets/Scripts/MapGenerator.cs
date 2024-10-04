b728ba196617f949d90b106f76c0d9a2094d332a
peterfgeorge <petegeo52@gmail.com>
2024-10-04 15:05:50 -0400

    Adds in colorMap option to renderer

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEditor;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode {NoiseMap, ColourMap}
+    public DrawMode drawMode;
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -15,14 +19,34 @@ public class MapGenerator : MonoBehaviour
 
     public int seed;
     public Vector2 offset;
-
+ 
     public bool autoUpdate;
+
+    public TerrainType[] regions;
 
     public void GenerateMap() {
         float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                float currentHeight = noiseMap [x, y];
+                for (int i = 0; i < regions.Length; i++) {
+                    if (currentHeight <= regions[i].height) {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
+
         MapDisplay display = FindObjectOfType<MapDisplay> ();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap) {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        } else if (drawMode == DrawMode.ColourMap) {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+        
     }
 
     void OnValidate() {
@@ -39,4 +63,11 @@ public class MapGenerator : MonoBehaviour
             octaves = 0;
         }
     }
+}
+
+[System.Serializable]
+public struct TerrainType {
+    public string name;
+    public float height;
+    public Color colour;
 }
